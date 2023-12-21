0b44e24d8f2af90bde97cb16dc85f26b923b1065
KelseyDang2002 <kelseydang813@gmail.com>
2023-12-21 11:29:10 -0800

    twoSum done

--- twoSum/main.cpp
+++ twoSum/main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-    cout << "Hello , World!" << endl;
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int size = sizeof(nums);
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if ((nums[i] + nums[j]) == target) {
+                printf("%u + %u = %u\n", nums[i], nums[j], target);
+            }
+        }
+    }
     return 0;
-}
+};
