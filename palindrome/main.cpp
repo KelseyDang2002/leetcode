f879df0cd4f71b091696af8b136580034eb6ef36
KelseyDang2002 <kelseydang813@gmail.com>
2024-01-07 11:44:28 -0800

    palindrome done

--- palindrome/main.cpp
+++ palindrome/main.cpp
@@ -3,5 +3,35 @@
 using namespace std;
 
 int main() {
-    return 0;
+    printf("\nPalindrome\n\n");
+
+    int x, num, remainder, reverse = 0;
+
+    printf("Enter an integer: ");
+    cin >> num;
+    cout << "You entered " << num << ".\n\n";
+
+    x = num;
+
+    if(x < 0) {
+        printf("Not a palindrome.\n\n");
+        return false;
+    }
+
+    while(num != 0) {
+        remainder = num % 10;
+        reverse = (reverse * 10) + remainder;
+        num = num / 10;
+    }
+
+    cout << "Reverse: " << reverse << ".\n\n";
+
+    if(reverse == x) {
+        printf("Palindrome.\n\n");
+        return true;
+    }
+    else {
+        printf("Not a palindrome.\n\n");
+        return false;
+    }
 }
